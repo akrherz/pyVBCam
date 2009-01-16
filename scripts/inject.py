2507d2bea0107e26a575f6f3cb3a22ef69dda5be
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-01-16 01:22:09 +0000

    fix

--- scripts/inject.py
+++ scripts/inject.py
@@ -7,4 +7,4 @@ files = glob.glob("*%s*" % (sys.argv[1],))
 for dir in files:
   print dir
   fn = (dir.split(".", 1)[1]).split("_")[:2]
-  os.system("/home/ldm/bin/pqinsert -p 'lapse c 000000000000 KCCI/%s.qt BOGUS qt' %s/out.mov" % (fn, dir) )
+  os.system("/home/ldm/bin/pqinsert -p 'lapse c 000000000000 KCCI/%s.qt BOGUS qt' %s/out.mov" % ("_".join(fn), dir) )
