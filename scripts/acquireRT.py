a37f61a7e056f54d41af5515d848fd8fec23b305
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-06-11 14:13:13 +0000

    add a turn off feature

--- scripts/acquireRT.py
+++ scripts/acquireRT.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 from pyIEM import cameras
-import os
+import os, mx.DateTime
+
+now = mx.DateTime.now()
 
 for cid in cameras.cams.keys():
   if not cameras.cams[cid]['online']:
+    continue
+
+  if cid == "KCRG-014" and (now.hour > 20 or now.hour < 6):
     continue
 
   os.system("./getStill4web.py %s &" % (cid,) )
