7c84f0ffe4a6b977812038c6326dbc2aff287998
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-07-22 16:23:47 +0000

    turn off corn cam earlier

--- scripts/acquireRT.py
+++ scripts/acquireRT.py
@@ -9,7 +9,7 @@ for cid in cameras.cams.keys():
   if not cameras.cams[cid]['online']:
     continue
 
-  if cid == "KCRG-014" and (now.hour > 20 or now.hour < 6):
+  if cid == "KCRG-014" and (now.hour > 19 or now.hour < 6):
     continue
 
   os.system("./getStill4web.py %s &" % (cid,) )
