555aba62e3165559060bd41bd41deb2db006b791
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-08-16 20:15:36 +0000

    make interval dynamic and set in the database

--- scripts/acquireRT.py
+++ scripts/acquireRT.py
@@ -1,8 +1,16 @@
 
 from pyIEM import cameras
 import os, mx.DateTime
+import secret
+import pg, sys
+mesosite = pg.connect('mesosite', host=secret.DBHOST)
 
 now = mx.DateTime.now()
+
+# Figure out how frequently we are to update
+rs = mesosite.query("SELECT * from properties WHERE propname = 'webcam.interval'").dictresult()
+if rs[0]['propvalue'] == "300" and now.minute % 5 != 0:
+  sys.exit(0)
 
 for cid in cameras.cams.keys():
   if not cameras.cams[cid]['online']:
