22b02bae448a5cd583667001e5f349409114db22
akrherz <akrherz@iastate.edu>
2012-10-26 11:22:01 -0500

    don't make noise when database goes away

--- scripts/acquireRT.py
+++ scripts/acquireRT.py
@@ -1,16 +1,21 @@
-
+"""
+Fire off the aquistion of RT webcam images
+"""
 
 import os, mx.DateTime
 import secret
 import pg, sys
-mesosite = pg.connect('mesosite', host=secret.DBHOST)
+try:
+    mesosite = pg.connect('mesosite', host=secret.DBHOST)
+except:
+    sys.exit(0)
 
 now = mx.DateTime.now()
 
 # Figure out how frequently we are to update
 rs = mesosite.query("SELECT * from properties WHERE propname = 'webcam.interval'").dictresult()
 if rs[0]['propvalue'] == "300" and now.minute % 5 != 0:
-  sys.exit(0)
+    sys.exit(0)
 
 rs = mesosite.query("""SELECT id from webcams WHERE 
     online = 't' and network in ('KELO','KCCI','KCRG','ISUC') """).dictresult()
