79bfd1d8cc0c23b10631bf5eb3d587cd6a44fe71
akrherz <akrherz@iastate.edu>
2015-07-28 09:54:10 -0500

    pep8 cleanups, fix timestamp placement, make slightly smaller

--- scripts/longLapse.py
+++ scripts/longLapse.py
@@ -3,11 +3,13 @@ import common
 
 import time
 import logging
-import StringIO, mx.DateTime
+import StringIO
+import mx.DateTime
 from PIL import Image, ImageDraw, ImageFont
-import sys, os
+import sys
+import os
 
-fontsize = 22
+fontsize = 18
 font = ImageFont.truetype('../lib/veramono.ttf', fontsize)
 
 os.chdir("../tmp/")
@@ -18,36 +20,33 @@ mydir = "longterm.%s.%s" % (site, mx.DateTime.now().strftime("%Y%m%d%H%M%S"))
 os.makedirs(mydir)
 os.chdir(mydir)
 
-logging.basicConfig(filename="%s.log"%(site,),filemode='w' )
+logging.basicConfig(filename="%s.log" % (site,), filemode='w')
 
 c = common.get_vbcam(site)
-logging.info("Camera Settings: %s" % ( c.settings, ) )
+logging.info("Camera Settings: %s" % (c.settings, ))
 
 i = 0
 while i < 100000:
-    logging.info("i = %s" % (i,) )
+    logging.info("i = %s" % (i,))
 
     # Set up buffer for image to go to
     buf = StringIO.StringIO()
 
-    #c.panDrct(90 + (.25 * i))
-    #c.panDrct( dirs[i] )
     drct = c.getDirection()
-    buf.write( c.getOneShot() )
+    buf.write(c.getOneShot())
     buf.seek(0)
-    i0 = Image.open( buf )
+    i0 = Image.open(buf)
 
     now = mx.DateTime.now()
-    s = "%3s %8s" % (c.drct2txt(drct), now.strftime("%-I:%M %p") )
+    s = "%3s %8s" % (c.drct2txt(drct), now.strftime("%-I:%M %p"))
     (w, h) = font.getsize(s)
 
     draw = ImageDraw.Draw(i0)
-    draw.rectangle( [215-w-10,370,215,370+fontsize], fill="#000000" )
-    draw.text((210-w,370), s, font=font)
+    draw.rectangle([205 - w - 10, 370, 205, 370 + h], fill="#000000")
+    draw.text((200 - w, 370), s, font=font)
     del draw
 
-
-    i0.save('%05i.jpg' % ( i,))
+    i0.save('%05i.jpg' % (i, ))
     del i0
     del buf
     time.sleep(int(sys.argv[2]))
