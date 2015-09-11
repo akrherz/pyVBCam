4863451f15021c3cd36db62f43aa70547c2fc452
akrherz <akrherz@iastate.edu>
2015-09-11 10:17:37 -0500

    pep8 fixes and label placement on large images

--- scripts/getStill4web.py
+++ scripts/getStill4web.py
@@ -10,7 +10,6 @@ import subprocess
 import sys
 import datetime
 import urllib2
-import logging
 from PIL import Image, ImageDraw, ImageFont
 import pytz
 import psycopg2.extras
@@ -20,8 +19,8 @@ cursor = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 sys.path.insert(0, "../vbcam")
 import vbcam
 
-def camRunner( cid ):
-# Who am I running for
+def camRunner(cid):
+    # Who am I running for
     cursor.execute("""SELECT * from webcams where id = %s """, (cid,))
     row = cursor.fetchone()
     now = datetime.datetime.now()
@@ -33,7 +32,7 @@ def camRunner( cid ):
 
         # Get Still
         buf = StringIO.StringIO()
-        buf.write( cam.getOneShot() )
+        buf.write(cam.getOneShot())
         buf.seek(0)
 
     else:
@@ -47,59 +46,61 @@ def camRunner( cid ):
             return
         modified = req2.info().getheader('Last-Modified')
         if modified:
-            gmt = datetime.datetime.strptime(modified, "%a, %d %b %Y %H:%M:%S %Z")
+            gmt = datetime.datetime.strptime(modified,
+                                             "%a, %d %b %Y %H:%M:%S %Z")
             now = gmt + datetime.timedelta(seconds=now.utcoffset().seconds)
             # Round up to nearest 5 minute bin
             roundup = 5 - now.minute % 5
             gmt += datetime.timedelta(minutes=roundup)
-        buf = StringIO.StringIO( req2.read() )
+        buf = StringIO.StringIO(req2.read())
         buf.seek(0)
     if buf.len == 0:
         return
-    i0 = Image.open( buf )
+    i0 = Image.open(buf)
     # 320x240 variant
     try:
-        i320 = i0.resize((320, 240), Image.ANTIALIAS) 
+        i320 = i0.resize((320, 240), Image.ANTIALIAS)
     except:
         slaughter()
         return
 
     # Get direction cam is looking
     if row['scrape_url'] is None:
-        drct = cam.getDirection() 
-        drctTxt = cam.drct2txt( drct )
+        drct = cam.getDirection()
+        drctTxt = cam.drct2txt(drct)
     else:
         drct = row['pan0']
-        drctTxt = vbcam.drct2dirTxt( row['pan0'] )
-  
+        drctTxt = vbcam.drct2dirTxt(row['pan0'])
+
     # Place timestamp/direction on image
     font = ImageFont.truetype('../lib/veramono.ttf', 10)
 
-
     draw = ImageDraw.Draw(i0)
-    text = "(%s) %s %s" % (drctTxt, row['name'], 
-                          now.strftime("%-2I:%M:%S %p - %d %b %Y") )
+    text = "(%s) %s %s" % (drctTxt, row['name'],
+                           now.strftime("%-2I:%M:%S %p - %d %b %Y"))
     (w, h) = font.getsize(text)
-    draw.rectangle( [5,475-h,5+w,475], fill="#000000" )
-    draw.text((5,475-h), text, font=font)
+    # Our image may not be 640x480 at full res
+    (_, imgheight) = i0.size
+    draw.rectangle([5, imgheight - 5 - h, 5+w, imgheight - 5], fill="#000000")
+    draw.text((5, imgheight - 5 - h), text, font=font)
 
     # Save 640x480
-    i0.save("../tmp/%s-640x480.jpg" % (cid,) )
+    i0.save("../tmp/%s-640x480.jpg" % (cid,))
 
     draw = ImageDraw.Draw(i320)
-    text = "(%s) %s %s" % (drctTxt, row['name'], now.strftime("%-2I:%M:%S %p - %d %b %Y") )
+    text = "(%s) %s %s" % (drctTxt, row['name'],
+                           now.strftime("%-2I:%M:%S %p - %d %b %Y"))
     (w, h) = font.getsize(text)
-    draw.rectangle( [5,235-h,5+w,235], fill="#000000" )
-    draw.text((5,235-h), text, font=font)
+    draw.rectangle([5, 235-h, 5+w, 235], fill="#000000")
+    draw.text((5, 235-h), text, font=font)
 
     # Save 640x480
-    i320.save("../tmp/%s-320x240.jpg" % (cid,) )
+    i320.save("../tmp/%s-320x240.jpg" % (cid,))
 
     # Save direction to database
     sql = "INSERT into camera_log(cam, valid, drct) values (%s,%s,%s)"
     args = (cid, now.strftime('%Y-%m-%d %H:%M'), drct)
     cursor.execute(sql, args)
-
 
     sql = "DELETE from camera_current WHERE cam = %s"
     args = (cid,)
@@ -110,35 +111,45 @@ def camRunner( cid ):
     cursor.execute(sql, args)
 
     # Put into LDM
-    cmd = "/home/ldm/bin/pqinsert -p 'webcam c %s camera/stills/%s.jpg camera/%s/%s_%s.jpg jpg' ../tmp/%s-320x240.jpg" % (gmt.strftime("%Y%m%d%H%M"), cid, cid, cid, gmt.strftime("%Y%m%d%H%M"), cid)
+    cmd = ("/home/ldm/bin/pqinsert "
+           "-p 'webcam c %s camera/stills/%s.jpg camera/%s/%s_%s.jpg jpg'"
+           " ../tmp/%s-320x240.jpg") % (gmt.strftime("%Y%m%d%H%M"), cid, cid,
+                                        cid, gmt.strftime("%Y%m%d%H%M"), cid)
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
+                            stderr=subprocess.PIPE)
     proc.stderr.read()
-    cmd = "/home/ldm/bin/pqinsert -p 'webcam ac %s camera/640x480/%s.jpg camera/%s/%s_%s.jpg jpg' ../tmp/%s-640x480.jpg" % (gmt.strftime("%Y%m%d%H%M"), cid, cid, cid, gmt.strftime("%Y%m%d%H%M"), cid )
+    cmd = ("/home/ldm/bin/pqinsert "
+           "-p 'webcam ac %s camera/640x480/%s.jpg camera/%s/%s_%s.jpg jpg'"
+           " ../tmp/%s-640x480.jpg") % (gmt.strftime("%Y%m%d%H%M"), cid, cid,
+                                        cid, gmt.strftime("%Y%m%d%H%M"), cid)
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
+                            stderr=subprocess.PIPE)
     proc.stderr.read()
 
     slaughter()
+
 
 def slaughter():
     cursor.close()
     dbconn.commit()
     dbconn.close()
     killer()
-    
+
+
 def killer():
     os.kill(os.getpid(), 9)
+
 
 def eb():
     pass
 
-cid = sys.argv[1]
-reactor.callInThread( camRunner, cid )
+if __name__ == '__main__':
+    cid = sys.argv[1]
+    reactor.callInThread(camRunner, cid)
 
-# You have 1 minute to finish :)
-reactor.callLater(57, slaughter)
-reactor.callLater(59, killer)
+    # You have 1 minute to finish :)
+    reactor.callLater(57, slaughter)
+    reactor.callLater(59, killer)
 
-# Go reactor
-reactor.run()
+    # Go reactor
+    reactor.run()
