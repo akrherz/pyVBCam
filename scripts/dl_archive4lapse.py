e3ca6cbf9ac02d7871aa87849e4278d8bc81bd0a
akrherz <akrherz@iastate.edu>
2017-08-23 10:44:17 -0500

    pylint and do bessey hall lapse

--- scripts/dl_archive4lapse.py
+++ scripts/dl_archive4lapse.py
@@ -1,27 +1,37 @@
 """
 Download the frames archived to produce a longterm lapse
 """
-import requests
+from __future__ import print_function
 import datetime
 
-sts = datetime.datetime(2016, 9, 22)
-ets = datetime.datetime(2016, 9, 25)
-interval = datetime.timedelta(minutes=5)
+import requests
 
-i = 0
-now = sts
-while now < ets:
-    uri = now.strftime(("http://mesonet.agron.iastate.edu/archive/"
-                        "data/%Y/%m/%d/camera/KCRG-022/"
-                        "KCRG-022_%Y%m%d%H%M.jpg"))
-    req = requests.get(uri)
-    if req.status_code == 200:
-        o = open("/mesonet/tmp/frames/%05d.jpg" % (i,), 'w')
-        o.write(req.content)
-        o.close()
-        i += 1
-    else:
-        print("%s -> %s" % (uri, req.status_code))
-    uri = now
 
-    now += interval
+def main():
+    """Go!"""
+    sts = datetime.datetime(2016, 3, 14, 13)  # Run 13z to 0z
+    ets = datetime.datetime(2017, 8, 23)
+    interval = datetime.timedelta(minutes=30)
+
+    i = 0
+    now = sts
+    while now < ets:
+        uri = now.strftime(("http://mesonet.agron.iastate.edu/archive/"
+                            "data/%Y/%m/%d/camera/ISUC-002/"
+                            "ISUC-002_%Y%m%d%H%M.jpg"))
+        req = requests.get(uri)
+        if req.status_code == 200:
+            fp = open("/mesonet/tmp/frames/%05d.jpg" % (i,), 'w')
+            fp.write(req.content)
+            fp.close()
+            i += 1
+        else:
+            print("%s -> %s" % (uri, req.status_code))
+        if now.hour < 13:
+            now += datetime.timedelta(hours=13)
+        else:
+            now += interval
+
+
+if __name__ == '__main__':
+    main()
