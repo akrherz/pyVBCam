cf96234862eae5ab40e287fb179260ea222fbd20
akrherz <akrherz@iastate.edu>
2023-09-20 08:55:04 -0500

    refactor and test lapse

---
+++ tests/test_lapse.py
@@ -0,0 +1,17 @@
+"""Test the lapse module."""
+from io import BytesIO
+
+from pyvbcam import lapse
+
+
+def test_lapse_basic():
+    """Test the lapse module."""
+    lp = lapse.Lapse()
+    # Mock lp.camera
+    lp.camera = lapse.scrape(1, {"pan0": 0, "scrape_url": "http://localhost"})
+    lp.camera.getDirection = lambda: 0
+    with open("tests/data/white.png", "rb") as fh:
+        payload = fh.read()
+    lp.camera.get_one_shot = lambda: payload
+    with BytesIO() as buf:
+        lp.do_frame(buf)
