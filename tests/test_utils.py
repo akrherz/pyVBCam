97e54468fc4de3bb0887c5c404817cdc53602772
akrherz <akrherz@iastate.edu>
2023-09-23 06:02:52 -0500

    test: improve coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,12 @@
 from pyvbcam import utils
 
 
+def test_get_password():
+    """Test the fetching of password from settings.json"""
+    assert utils.get_password("KCCI-0001") == "..."
+    assert utils.get_password("KELO-0001") == "..."
+
+
 def test_get_camids():
     """Test that we can get camids."""
     utils.get_camids("KCCI")
