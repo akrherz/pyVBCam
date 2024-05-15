8d64d50fb7e5626a441beea16fbf66f1612bb596
akrherz <akrherz@iastate.edu>
2024-05-15 10:58:11 -0500

    fix: set explict dates for all is_daily

--- scripts/set_autolapse_times.py
+++ scripts/set_autolapse_times.py
@@ -1,21 +1,19 @@
-"""Update the database to set proper sunrise and sunset times"""
+"""Update the database to set proper times."""
 
 import datetime
 from zoneinfo import ZoneInfo
 
 import ephem
 from pyiem.database import get_dbconnc
+from pyiem.util import utc
 
 NOW = datetime.datetime.now().replace(tzinfo=ZoneInfo("America/Chicago"))
 
 
 def mydate(tstamp):
     """Convert this string to a proper timestamp"""
-    if tstamp is None or tstamp == "":
-        return datetime.datetime(1989, 1, 1)
-
     gts = datetime.datetime.strptime(str(tstamp), "%Y/%m/%d %H:%M:%S")
-    return gts + datetime.timedelta(seconds=NOW.utcoffset().seconds)
+    return gts.replace(tzinfo=datetime.timezone.utc)
 
 
 def main():
@@ -28,7 +26,7 @@ def main():
     cursor.execute(
         """
         SELECT id, ST_x(geom) as lon, ST_y(geom) as lat from webcams WHERE
-        online = 't' and network in ('KELO','KCCI','KCRG','ISUC', 'MCFC')
+        online = 't' and network in ('KCCI','KCRG','ISUC', 'MCFC')
         """
     )
 
@@ -36,47 +34,70 @@ def main():
         ob = ephem.Observer()
         ob.lat = str(row["lat"])
         ob.long = str(row["lon"])
-        ob.date = ("%s 10:00") % (
-            datetime.datetime.utcnow().strftime("%Y/%m/%d"),
-        )
+        ob.date = f"{utc():%Y/%m/%d} 08:00"  # UTC
         sun.compute(ob)
-        # print row['id'], sun.rise_time, sun.set_time
         r2 = mydate(ob.next_rising(sun))
         s2 = mydate(ob.next_setting(sun))
 
-        sql = """
-            UPDATE webcam_scheduler SET
-            begints = '%s'::timestamp - '45 minutes'::interval,
-            endts = '%s'::timestamp + '45 minutes'::interval WHERE
-            cid = '%s' and filename ~* '_eve'
-        """ % (
-            s2.strftime("%Y-%m-%d %H:%M"),
-            s2.strftime("%Y-%m-%d %H:%M"),
-            row["id"],
+        # set morning and afternoon
+        cursor2.execute(
+            """
+            update webcam_scheduler set begints = %s, endts = %s
+            where cid = %s and strpos(filename, '_morning') > 0
+            and is_daily
+            """,
+            (
+                NOW.replace(hour=8, minute=0, second=0, microsecond=0),
+                NOW.replace(hour=11, minute=0, second=0, microsecond=0),
+                row["id"],
+            ),
         )
-        cursor2.execute(sql)
-        sql = """
-            UPDATE webcam_scheduler SET
-            begints = '%s'::timestamp - '30 minutes'::interval,
-            endts = '%s'::timestamp + '45 minutes'::interval WHERE
-            cid = '%s' and filename ~* '_sunrise'
-        """ % (
-            r2.strftime("%Y-%m-%d %H:%M"),
-            r2.strftime("%Y-%m-%d %H:%M"),
-            row["id"],
+        cursor2.execute(
+            """
+            update webcam_scheduler set begints = %s, endts = %s
+            where cid = %s and strpos(filename, '_afternoon') > 0
+            and is_daily
+            """,
+            (
+                NOW.replace(hour=13, minute=0, second=0, microsecond=0),
+                NOW.replace(hour=16, minute=0, second=0, microsecond=0),
+                row["id"],
+            ),
         )
-        cursor2.execute(sql)
-        sql = """
-            UPDATE webcam_scheduler SET
-            begints = '%s'::timestamp - '30 minutes'::interval,
-            endts = '%s'::timestamp + '30 minutes'::interval WHERE
-            cid = '%s' and filename ~* '_day'
-        """ % (
-            r2.strftime("%Y-%m-%d %H:%M"),
-            s2.strftime("%Y-%m-%d %H:%M"),
-            row["id"],
+
+        cursor2.execute(
+            """
+            UPDATE webcam_scheduler SET begints = %s, endts = %s WHERE
+            cid = %s and filename ~* '_eve'
+        """,
+            (
+                s2 - datetime.timedelta(minutes=45),
+                s2 + datetime.timedelta(minutes=45),
+                row["id"],
+            ),
         )
-        cursor2.execute(sql)
+        cursor2.execute(
+            """
+            UPDATE webcam_scheduler SET begints = %s, endts = %s WHERE
+            cid = %s and filename ~* '_sunrise'
+        """,
+            (
+                r2 - datetime.timedelta(minutes=30),
+                r2 + datetime.timedelta(minutes=45),
+                row["id"],
+            ),
+        )
+        cursor2.execute(
+            """
+            UPDATE webcam_scheduler SET begints = %s, endts = %s WHERE
+            cid = %s and filename ~* '_day'
+        """,
+            (
+                r2 - datetime.timedelta(minutes=30),
+                s2 + datetime.timedelta(minutes=30),
+                row["id"],
+            ),
+        )
 
     cursor2.close()
     dbconn.commit()
